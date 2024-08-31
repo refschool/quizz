f5b3dbcfd4ce35240a7a7f09c8fac78e2073647a
Yvon Huynh <yvon.huynh@gmail.com>
2024-08-31 16:10:10 +0200

    score computation ok

--- front/poll.js
+++ front/poll.js
@@ -7,7 +7,7 @@ const closeBtn = document.querySelector('#closeBtn')
 let globalReference
 const result = document.getElementById('result');
 
-let score
+let score = 0
 
 let hashes = []
 let hashs = '269d96175e89df9fb809b53fd838d8073298b6877c4cbd07311d8dbe41c6a0c3' // single
@@ -110,31 +110,32 @@ validateButton.addEventListener('click', function (event) {
         endButton.style.display = 'block'
     }
 
-
-
     // get questionData from localStorage
     let questionData = JSON.parse(localStorage.getItem("questionData"))
     // get ids des input checkés
     // balayer toutes les réponses du DOM et regarder si l'id est dans correct list, coché + non == faux, coché + oui == juste
     let checkedBoxes = document.querySelectorAll('input[type="checkbox"]');
+    let merit = false;
 
     [...checkedBoxes].map(item => {
         let found = questionData.find(el => el.id == item.value)
 
+
         if (found.correct === 1 && item.checked === true) {
             item.parentElement.style.backgroundColor = 'green'
-            score++
+            merit = true
         } else if (found.correct === 0 && item.checked === true) {
             item.parentElement.style.backgroundColor = 'red'
+            merit = false
         }
         else if (found.correct === 1 && item.checked === false) {
             item.parentElement.style.borderColor = 'green'
+            merit = false
         }
     })
-
-
-
-
+    //update score
+    if (merit === true) score++
+    console.log("score", score)
 })
 
 
